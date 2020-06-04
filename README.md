0ff6b2179f5d1b0ec8e0c9e10185b592cfbefb3f
baranugur <baranugur7@gmail.com>
2020-06-04 20:45:23 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# BLG458E - Functional Programming
+
+## Term Project
+
+**Group 8**
+
+- İsmet Ata Yardımcı (150150151)
+- Lütfi Baran Uğur (150150081)
+- Mateo Hudhra (150160911)
---
+++ cse.hs
@@ -0,0 +1,19 @@
+data Ninja = Ninja {name:: String, country:: Char,
+                    status:: String, exam1:: Float,
+                    exam2:: Float, ability1:: String,
+                    ability2:: String, r:: Int}
+
+fire :: [Ninja]
+fire = []
+
+lightning :: [Ninja]
+lightning = []
+
+water :: [Ninja]
+water = []
+
+wind :: [Ninja]
+wind = []
+
+earth :: [Ninja]
+earth = []
