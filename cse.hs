08fd8c6fa2085b2ca8b7ca6b7c68b81620f09815
baranugur <baranugur7@gmail.com>
2020-06-06 18:17:17 +0300

    Separate ninjas into their respective nations

--- cse.hs
+++ cse.hs
@@ -2,39 +2,58 @@ import System.Environment
 import System.IO
 import Data.Char
 
-data Ninja = Ninja {name:: String, country:: Char,
-                    status:: String, exam1:: Float,
-                    exam2:: Float, ability1:: String,
-                    ability2:: String, r:: Int} deriving (Show)
+-- TODO: Create a 'class' for abilities.
+-- TODO: Calculate scores.
 
-fire :: [Ninja]
-fire = []
-
-lightning :: [Ninja]
-lightning = []
-
-water :: [Ninja]
-water = []
-
-wind :: [Ninja]
-wind = []
-
-earth :: [Ninja]
-earth = []
+data Ninja = Ninja {name :: String, country :: Char,
+                    status :: String, exam1 :: Float,
+                    exam2 :: Float, ability1 :: String,
+                    ability2 :: String, r :: Int} deriving (Show)
 
 -- For checking if the correct number of command line arguments are given.
 checkCommandLineArgs :: Int -> IO ()
 checkCommandLineArgs 1 = return ()
 checkCommandLineArgs _ = error "Usage: ./<executable> <filename>.txt"
 
+-- For parsing the country data.
+parseCountry :: String -> Char
+parseCountry country
+    | country == "Fire"      = 'F'
+    | country == "Lightning" = 'L'
+    | country == "Water"     = 'W'
+    | country == "Wind"      = 'N'
+    | country == "Earth"     = 'E'
+    | otherwise              = error "Invalid country."
+
 -- For reading the ninja information and returning a ninja.
 readNinja :: [String] -> Ninja
-readNinja [name, country, exam1, exam2, ability1, ability2] = Ninja name (toUpper $ head country) "junior" (read exam1 :: Float) (read exam2 :: Float) ability1 ability2 0
+readNinja [name, country, exam1, exam2, ability1, ability2] = Ninja name (parseCountry country) "junior" (read exam1 :: Float) (read exam2 :: Float) ability1 ability2 0
 
 -- For reading all the information about the ninjas and returning a list of ninjas.
 readNinjas :: [String] -> [Ninja]
 readNinjas []           = []
 readNinjas (stat:stats) = (readNinja $ words stat):(readNinjas stats)
+
+getNinjas :: [Ninja] -> Char -> [Ninja]
+getNinjas ninjas c = filter (\ninja -> (country ninja == c)) ninjas
+
+getFire :: [Ninja] -> [Ninja]
+getFire ninjas = getNinjas ninjas 'F'
+
+getLigthning :: [Ninja] -> [Ninja]
+getLigthning ninjas = getNinjas ninjas 'L'
+
+getWater :: [Ninja] -> [Ninja]
+getWater ninjas = getNinjas ninjas 'W'
+
+getWind :: [Ninja] -> [Ninja]
+getWind ninjas = getNinjas ninjas 'N'
+
+getEarth :: [Ninja] -> [Ninja]
+getEarth ninjas = getNinjas ninjas 'E'
+
+separate :: [Ninja] -> [[Ninja]]
+separate ninjas = [getFire ninjas, getLigthning ninjas, getWater ninjas, getWind ninjas, getEarth ninjas]
 
 main :: IO ()
 main = do
@@ -49,4 +68,7 @@ main = do
     -- Create a list of ninjas.
     let stats = lines content
     let ninjas = readNinjas stats
-    print ninjas
+
+    -- Create nations.
+    let [fire, lightning, water, wind, earth] = separate ninjas
+    print wind
